886667bd91df92159ecc689c203433558bdb390f
Ars Vladimirov <grokhael@gmail.com>
2024-02-15 20:15:38 +0600

    add  imports

--- src/main.py
+++ src/main.py
@@ -14,10 +14,21 @@ from supervisely.io.fs import (
     list_files_recursively,
 )
 
+from pathlib import Path
 
 import src.globals as g
 import src.utils as u
 import supervisely as sly
+from fastapi import Response, HTTPException
+from supervisely.app.widgets import Container
+from src.ui.input import card_1
+
+
+layout = Container(widgets=[card_1], direction="vertical")
+
+static_dir = Path(g.STORAGE_DIR)
+app = sly.Application(layout=layout, static_dir=static_dir)
+server = app.get_server()
 
 
 @server.get("/get-stats", response_class=Response)
