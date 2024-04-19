8ccb445a7504f368e382721241f118e8b9ff8ea9
Ars Vladimirov <grokhael@gmail.com>
2024-04-19 16:07:23 +0500

    add dtools stats-cache control

--- src/globals.py
+++ src/globals.py
@@ -25,5 +25,5 @@ TF_ACTIVE_REQUESTS_DIR = f"{TF_STATS_DIR}/_active_requests"
 
 CHUNK_SIZE: int = 1000
 MINIMUM_DTOOLS_VERSION: str = (
-    "0.0.61"  # force stats to fully recalculate (f.e. when added new statistics)
+    "0.0.62"  # force stats to fully recalculate (f.e. when added new statistics)
 )
