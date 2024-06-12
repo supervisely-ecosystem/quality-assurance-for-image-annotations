f61d024b0c3cc8311df88018c44b39e3c36aa3a5
Ars Vladimirov <grokhael@gmail.com>
2024-06-12 16:12:14 +0500

    chunk_size -> 1000

--- src/globals.py
+++ src/globals.py
@@ -18,7 +18,7 @@ ACTIVE_REQUESTS_DIR = f"{STORAGE_DIR}/_active_requests"
 sly.fs.mkdir(ACTIVE_REQUESTS_DIR, remove_content_if_exists=True)
 TF_ACTIVE_REQUESTS_DIR = f"{TF_STATS_DIR}/_active_requests"
 
-CHUNK_SIZE: int = 3
+CHUNK_SIZE: int = 1000
 MINIMUM_DTOOLS_VERSION: str = (
     "0.0.88"  # force stats to fully recalculate (f.e. when edit statistics)
 )
